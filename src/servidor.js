872b61669e278eecb56c99b2d504c418a94f66e1
Jp <jgoes@cervejariacidadeimperial.com.br>
2025-06-30 17:13:10 -0300

    commit

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,9 @@
 import e from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+import { text } from "stream/consumers";
 
 const app = e();
 
@@ -12,6 +15,14 @@ const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 // console.log(diretorioPublico);
 app.use(e.static(diretorioPublico));
 
-app.listen(3000, () => {
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(3000, () => {
   console.log(`Servidor escutando na porta ${PORT}`);
+});
+
+const io = new Server(servidorHttp);
+
+io.on("connection", (socket) => {
+  console.log("Um cliente se conectou com o ID:", socket.id);
 });
