442dfe44ca82afd580d637f3c3a3a8fd9e6dd4ca
Jp <jgoes@cervejariacidadeimperial.com.br>
2025-07-09 14:36:23 -0300

    ajeitando

--- src/servidor.js
+++ src/servidor.js
@@ -15,7 +15,6 @@ const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(e.static(diretorioPublico));
 
 const servidorHttp = http.createServer(app);
-const servidorHttp2 = http.createServer(app);
 
 servidorHttp.listen(3000, () => {
   console.log(
@@ -23,14 +22,6 @@ servidorHttp.listen(3000, () => {
   );
 });
 
-servidorHttp2.listen(5000, () =>
-  console.log(`Servidor escutando na porta 5000`)
-);
-
-const io = new Server(servidorHttp, {
-  cors: {
-    origin: ["http://localhost:5000", "http://localhost:5001"],
-  },
-});
+const io = new Server(servidorHttp);
 
 export default io;
